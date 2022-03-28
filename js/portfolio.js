81acb8ca505a3a7b704ff601b1533b4f1dd88f88
rahimtbls <rahim.trbl69@gmail.com>
2022-03-28 17:10:29 +0200

    Scroll

--- js/portfolio.js
+++ js/portfolio.js
@@ -253,15 +253,6 @@ function bodyScrollingToggle() {
 
 }) ();
 
-/*(() =>{
-    const sections = document.querySelectorAll(".section");
-    sections.forEach((section) =>{
-        if(!section.classList.contains("active"))
-        {
-            section.classList.add("hide");
-        }
-    })
-})();*/
 
 window.addEventListener("load", () =>{
     document.querySelector(".preloader").classList.add("fade-out");
